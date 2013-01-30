ec35853f1ac1bd6a76665c199d1293c5df9bb3b2
seektan@gmail.com <seektan@gmail.com@3e29575a-350e-de9e-c50b-6d3c36ea11f7>
2013-01-30 06:24:51 +0000

    inline style

--- remix/remix.js
+++ remix/remix.js
@@ -1,14 +1,47 @@
 remix();
 function remix(){
 
-	if(!document.getElementById("remixCss")){
+	/*if(!document.getElementById("remixCss")){
 		var cssLink = document.createElement('link');
 			cssLink.rel  = 'stylesheet';
 			cssLink.type = 'text/css';
 			cssLink.id = 'remixCss';
 			cssLink.href = 'http://appapi.googlecode.com/svn/trunk/remix/remix.css?t='+(+new Date);
 		document.getElementsByTagName('head')[0].appendChild(cssLink);
+	}*/
+
+if(!document.getElementById("remixCss")){
+	var remixCss = document.createElement('style');
+		remixCss.type = 'text/css';
+		remixCss.id = 'remixStyle';
+
+	var styles = "body,html{height:100%;} \
+		#remix{overflow-y:auto;overflow-x:hidden;background:#000;} \
+		#remix li{*display:inline;*zoom:1;} \
+		.copycat{position:absolute;opacity:1;-webkit-transition:top 2s ease-out, opacity 1s;} \
+		.remixWrap body{overflow:hidden;} \
+		.remixWrap .remix{bottom:0;opacity:1;} \
+		.remix{position:fixed;z-index:65536;bottom:-100%;left:0;width:100%;height:100%;opacity:0;overflow:hidden;text-align:center;background:rgba(0,0,0,0.8);-webkit-transition:all 2s ease-in;} \
+		.remix .rlist{padding:10px 0;} \
+		.remix .ritem{display:inline-block;vertical-align:top;width:200px;height:200px;padding:10px;position:relative;} \
+		.remix .ritem a{color:#fff;text-align:left;} \
+		.remix .ritem span{position:absolute;background:rgba(0,0,0,0.7);padding:4px 6px;filter:alpha(opacity=0);opacity:0;-webkit-transition:opacity 0.2s;} \
+		.remix .rphoto{display:block;width:200px;height:200px;box-shadow:0 0 0 2px #fff;} \
+		.remix .rtitle{width:188px;left:10px;bottom:20px;} \
+		.remix .rprice{top:20px;left:10px;color:#fc0;} \
+		.remix .roffered{top:20px;right:10px;white-space:nowrap;} \
+		.remix .ritem a:hover span{filter:alpha(opacity=100);opacity:1;} \
+		.remix .ritem a:hover .rphoto{box-shadow:0 0 0 2px #f60;}";
+
+	if (remixCss.styleSheet){
+		remixCss.styleSheet.cssText = styles;
+	}else{
+		remixCss.appendChild(document.createTextNode(styles));
 	}
+}
+
+document.getElementsByTagName("head")[0].appendChild(css);
+
 
 	var imgs = document.getElementsByTagName("img"),
 		data = [],
