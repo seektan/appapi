7a95c42ca1896ac49b2e4a061eaf77e11cbe0feb
seektan@gmail.com <seektan@gmail.com@3e29575a-350e-de9e-c50b-6d3c36ea11f7>
2013-01-30 07:34:20 +0000

    ""

--- trunk/remix/remix.js
+++ trunk/remix/remix.js
@@ -24,8 +24,8 @@ function remix(){
 			.remix .ritem a:hover span{filter:alpha(opacity=100);opacity:1;} \
 			.remix .ritem a:hover .rphoto{box-shadow:0 0 0 2px #f60;} \
 			.remix .rclose{position:absolute;top:10px;right:5px;width:50px;height:50px;border-radius:50%;border:4px solid #fff;background:#000;cursor:pointer;display:none;} \
-			.remix .rclose:after{content:"";position:absolute;top:8px;left:22px;width:6px;height:36px;background:#fff;-webkit-transform: rotate(45deg);} \
-			.remix .rclose:before{content:"";position:absolute;top:8px;left:22px;width:6px;height:36px;background:#fff;-webkit-transform: rotate(-45deg);} \
+			.remix .rclose:after{content:'';position:absolute;top:8px;left:22px;width:6px;height:36px;background:#fff;-webkit-transform: rotate(45deg);} \
+			.remix .rclose:before{content:'';position:absolute;top:8px;left:22px;width:6px;height:36px;background:#fff;-webkit-transform: rotate(-45deg);} \
 			.remix:hover .rclose{display:block;}";
 
 		if (remixCss.styleSheet){
